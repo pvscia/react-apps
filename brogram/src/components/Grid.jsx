cb6507a181a40fe563d5784de952d23b35b828af
patricia <patricia@korindo.co.id>
2025-12-01 15:49:03 +0700

    u

--- brogram/src/components/Grid.jsx
+++ brogram/src/components/Grid.jsx
@@ -1,13 +1,92 @@
+import { useEffect, useState } from "react";
 import { workoutProgram } from "../utils";
+import WorkoutCard from "./WorkoutCard";
 
 export default function Grid() {
+    const [savedWorkouts, setSavedWorkouts] = useState(null)
+    const [selectedWorkout, setSelectedWorkout] = useState(null)
+    const completedWorkouts = Object.keys(savedWorkouts || {}).filter((val) => {
+        const entry = savedWorkouts[val]
+        return entry.isComplete
+    })
+
+    function handleSave(index, data) {
+        // save to local storage
+        const newObj = {
+            ...savedWorkouts,
+            [index]: { ...data },
+            isComplete: !!data.isComplete || !!savedWorkouts?.[index]?.isComplete
+        }
+        setSavedWorkouts(newObj)
+        localStorage.setItem('brogram', JSON.stringify(newObj))
+        setSelectedWorkout(null)
+    }
+
+    function handleComplete(index, data) {
+        // complete a workout (modify completed status)
+        const newObj = { ...data }
+        newObj.isComplete = true;
+        handleSave(index, newObj)
+    }
+
+    useEffect(() => {
+        if (!localStorage) { return }
+        let savedData = {}
+        if (localStorage.getItem('brogram')) {
+            savedData = JSON.parse(localStorage.getItem('brogram'))
+        }
+        setSavedWorkouts(savedData)
+    }, [])
+
     return (
-        <div className="training-grid-plan">
+        <div className="training-plan-grid">
             {Object.keys(workoutProgram).map((workout, workoutIndex) => {
+                const type = workoutIndex % 3 === 0 ? 'Push' : workoutIndex % 3 === 1 ? 'Pull' : 'Legs'
+
+                const trainingPlan = workoutProgram[workoutIndex]
+
+                const dayNum = ((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : (workoutIndex + 1)
+                const icon = workoutIndex % 3 === 0 ? (
+                    <i className="fa-solid fa-dumbbell"></i>
+                ) : workoutIndex % 3 === 1 ? (
+                    <i className="fa-solid fa-weight-hanging"></i>
+                ) : (
+                    <i className="fa-solid fa-bolt"></i>
+                )
+
+
+                if (workoutIndex == selectedWorkout) {
+                    return (
+                        <WorkoutCard key={workoutIndex}
+                            trainingPlan={trainingPlan}
+                            workoutIndex={workoutIndex}
+                            type={type} icon={icon}
+                            dayNum={dayNum}
+                            handleComplete={handleComplete}
+                            handleSave={handleSave}
+                            savedWeights={savedWorkouts?.[workoutIndex]?.weights} />
+                    )
+                }
+
+                const isLocked = (workoutIndex === 0 ? false :
+                    !completedWorkouts.includes(`${workoutIndex - 1}`)
+                )
+
                 return (
-                    <button key={workoutIndex}>
+                    <button key={workoutIndex} className={'card plan-card ' + (savedWorkouts ? ' inactive ' : '')} onClick={() => {
+                        if(isLocked)return
+                        setSelectedWorkout(workoutIndex)
+                    }}>
                         <div className="plan-card-header">
-                            <p>Day {((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : (workoutIndex + 1)}</p>
+                            <p>Day {dayNum}</p>
+                            {isLocked ? (
+                                <i className="fa-solid fa-lock"></i>
+                            ) : (
+                                icon
+                            )}
+                        </div>
+                        <div className="plan-card-header">
+                            <h4><b>{type}</b></h4>
                         </div>
                     </button>
                 )
--- brogram/src/components/WorkoutCard.jsx
+++ brogram/src/components/WorkoutCard.jsx
@@ -1,7 +1,105 @@
-export default function WorkoutCard(){
-    return(
-        <>
-            
-        </>
+import React, { useState } from "react"
+import Modal from "./Modal"
+import { exerciseDescriptions } from "../utils"
+
+export default function WorkoutCard(props) {
+    const { trainingPlan, workoutIndex, type, dayNum, icon, savedWeights, handleSave, handleComplete } = props
+
+    const { warmup, workout } = trainingPlan || {}
+
+    const [showExcerciseDescription, setShowExerciseDescription] = useState(null)
+
+    const [weights, setWeights] = useState(savedWeights || {})
+
+    function handleAddWeight(title, weight) {
+        const newObj = {
+            ...weights,
+            [title]: weight
+        }
+
+        setWeights(newObj)
+    }
+
+    return (
+        <div className="workout-container">
+            {showExcerciseDescription && <Modal showExcerciseDescription={showExcerciseDescription} handleCloseModal={() => { setShowExerciseDescription(null) }} />}
+            <div className="workout-card card">
+                <div className="plan-card-header">
+                    <p>Day {dayNum}</p>
+                    {icon}
+                </div>
+                <div className="plan-card-header">
+                    <h2><b>{type} Workout</b></h2>
+                </div>
+            </div>
+
+            <div className="workout-grid">
+                <div className="exercise-name">
+                    <h4>Warmup</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+
+                {warmup.map((warmupExercise, warmupIndex) => {
+                    return (
+                        <React.Fragment key={warmupIndex}>
+                            <div className="exercise-name">
+                                <p>{warmupIndex + 1}. {warmupExercise.name}</p>
+                                <button className="help-icon" onClick={() => {
+                                    setShowExerciseDescription({ name: warmupExercise.name, description: exerciseDescriptions[warmupExercise.name] })
+                                }}>
+                                    <i className="fa-regular fa-circle-question"></i>
+                                </button>
+                            </div>
+                            <p className="exercise-info">{warmupExercise.sets}</p>
+                            <p className="exercise-info">{warmupExercise.reps}</p>
+                            <input className="weight-input" placeholder="N/A" disabled />
+                        </React.Fragment>
+                    )
+                })}
+
+            </div>
+
+            <div className="workout-grid">
+                <div className="excercise-name">
+                    <h4>Workout</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+
+                {workout.map((workoutExercise, workoutIdx) => {
+                    return (
+                        <React.Fragment key={workoutIdx}>
+                            <div className="exercise-name">
+                                <p>{workoutIdx + 1}. {workoutExercise.name}</p>
+                                <button className="help-icon" onClick={() => {
+                                    setShowExerciseDescription({ name: workoutExercise.name, description: exerciseDescriptions[workoutExercise.name] })
+                                }}>
+                                    <i className="fa-regular fa-circle-question"></i>
+                                </button>
+                            </div>
+                            <p className="exercise-info">{workoutExercise.sets}</p>
+                            <p className="exercise-info">{workoutExercise.reps}</p>
+                            <input type= "number" className="weight-input" placeholder="14" value={weights[workoutExercise.name] || ''} onChange={(e) => {
+                                handleAddWeight(workoutExercise.name, e.target.value)
+                            }} />
+                        </React.Fragment>
+                    )
+                })}
+
+            </div>
+
+
+            <div className="workout-buttons">
+                <button onClick={() => {
+                    handleSave(workoutIndex, { weights })
+                }}>Save & Exit</button>
+                <button onClick={() => {
+                    handleComplete(workoutIndex, { weights })
+                }} disabled={Object.keys(weights).length !== workout.length}>Complete</button>
+            </div>
+        </div>
     )
 }
